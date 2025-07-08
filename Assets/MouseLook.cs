5b0c9693261aaa261630f76ca00a791bc8c72d7c
Connor061254 <connor4148@gmail.com>
2025-07-08 16:28:17 -0700

    worked on looking around from the camera

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{ 
+    public float mouseSensitivity = 100f;
+    public Transform playerbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerbody.Rotate(Vector3.up * mouseX);
+    }
+}
