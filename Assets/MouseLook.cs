4d1bbbcadcaff11b7da27e5c5b7c2014627b03b8
Connor061254 <connor4148@gmail.com>
2025-07-08 23:00:43 -0700

    Added code allowing you to look around

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 { 
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 250f;
     public Transform playerbody;
+    float xRotation = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -15,7 +16,10 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90f, 90f);
         playerbody.Rotate(Vector3.up * mouseX);
-    }
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }   
 }
