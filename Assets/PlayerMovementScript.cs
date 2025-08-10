bdcfac1a3131fa81bfdef8042cbea41252dcd30b
Connor061254 <connor4148@gmail.com>
2025-08-09 18:33:46 -0700

    Reapply "fixed everything and made some changes to the pick up system. also add new ocean"
    This reverts commit aa48dff09e3fae999c19ed531f9100314ef692b5.

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -15,18 +15,18 @@ public class PlayerMovementScript : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-
+    public float groundCheckRadius = 0.2f; 
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
-        
-        if(isGrounded && velocity.y < 0)
+
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-    
+
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -35,13 +35,24 @@ public class PlayerMovementScript : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 
         }
         velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(velocity *Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    // This function draws a helpful visual sphere in the Scene view
+    // so you can see your groundCheck area.
+    void OnDrawGizmos()
+    {
+        if (groundcheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundcheck.position, groundCheckRadius);
+        }
     }
 }
