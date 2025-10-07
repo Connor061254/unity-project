626ed9f79921122844a9b22c48588d8406ccfd65
Connor061254 <connor4148@gmail.com>
2025-10-06 21:08:56 -0700

    added ID

---
+++ Assets/Scripts/Identification.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public enum CharacterType
+{
+    BeanstalkBill,
+    TubbsMcGee,
+    CutlassKate,
+
+}
+public class Identification : MonoBehaviour
+{
+
+    public CharacterType type;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
