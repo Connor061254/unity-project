f959a71a8ce25515ac9a0a63396e1434ab6f5bd9
Connor061254 <connor4148@gmail.com>
2026-01-02 02:51:52 -0700

    improved crouch

--- Assets/Scripts/playerCrouch.cs
+++ Assets/Scripts/playerCrouch.cs
@@ -10,7 +10,11 @@ public class playerCrouch : MonoBehaviour
 
     private float crouchCamera;
 
+    public float crouchCameraHeight = 0.4f;
+
     private float standingCamera;
+
+    public float crouchTransitionSpeed = 10f;
 
     public float WalkSpeed;
 
@@ -39,6 +43,8 @@ public class playerCrouch : MonoBehaviour
         crouchCenter = new Vector3(standingCenter.x, standingCenter.y - (standingHeight - crouchHeight) / 2f, standingCenter.z);
         
         crouchCamera = standingCamera - (standingHeight - crouchHeight);
+
+        Debug.Log(standingCamera);
     }
 
 
@@ -57,18 +63,26 @@ public class playerCrouch : MonoBehaviour
 
     void Crouch()
     {
-        characterController.height = crouchHeight;
-        characterController.center = crouchCenter;
+        characterController.height = Mathf.Lerp(characterController.height, crouchHeight, Time.deltaTime * crouchTransitionSpeed);
+        characterController.center = Vector3.Lerp(characterController.center, crouchCenter, Time.deltaTime * crouchTransitionSpeed);
         controller.walkSpeed = crouchSpeed;
-        playerCamera.transform.localPosition = new UnityEngine.Vector3(playerCamera.transform.localPosition.x, crouchCamera, playerCamera.transform.localPosition.z);
+
+        float currentCamY = playerCamera.transform.localPosition.y;
+        float newCamY = Mathf.Lerp(currentCamY, crouchCameraHeight, Time.deltaTime * crouchTransitionSpeed);
+        playerCamera.transform.localPosition = new UnityEngine.Vector3(playerCamera.transform.localPosition.x, newCamY, playerCamera.transform.localPosition.z);
+        Debug.Log("stand Y is" + standingCamera);
+        Debug.Log("Crouch Targer is :" + crouchCameraHeight);
         
     }
 
     void StandUp()
     {
-        characterController.height = standingHeight;
-        characterController.center = standingCenter;
-        playerCamera.transform.localPosition = new UnityEngine.Vector3 (playerCamera.transform.localPosition.x, standingCamera, playerCamera.transform.localPosition.z);
+        characterController.height = Mathf.Lerp(characterController.height, standingHeight, Time.deltaTime * crouchTransitionSpeed);
+        characterController.center = Vector3.Lerp(characterController.center, standingCenter, Time.deltaTime * crouchTransitionSpeed);
+
+        float currentCamY = playerCamera.transform.localPosition.y;
+        float newCamY = Mathf.Lerp(currentCamY, standingCamera, Time.deltaTime * crouchTransitionSpeed);
+        playerCamera.transform.localPosition = new UnityEngine.Vector3 (playerCamera.transform.localPosition.x, newCamY, playerCamera.transform.localPosition.z);
         controller.walkSpeed = WalkSpeed;
         
     }
