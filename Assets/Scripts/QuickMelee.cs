102d237c724abb41137df74c410c34a6775bf914
Connor061254 <connor4148@gmail.com>
2026-01-13 23:49:04 -0700

    working on adding a quick melee

---
+++ Assets/Scripts/QuickMelee.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuickMelee : MonoBehaviour
+{
+    float range = 2f;
+  
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Melee()
+    {
+        Vector3 attackPoint = transform.position + (transform.forward * range);
+        Collider[] hitColliders = Physics.OverlapSphere(attackPoint, 1f);
+
+        foreach (Collider hit in hitColliders)
+        {
+            if (hit.gameObject.GetComponent<HealthController>())
+            {
+                hit.gameObject.GetComponent<HealthController>().TakeDamage(100f);
+            }
+        }
+        
+    }
+
+    void OnMelee(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Melee();
+        }
+    }
+}
+
+
