54f1d6a43c630f18246ae2f33f2185fd193e332e
Connor061254 <connor4148@gmail.com>
2026-03-11 21:38:49 -0600

    added knockback to the cannon

--- Assets/Scripts/CannonWeapon.cs
+++ Assets/Scripts/CannonWeapon.cs
@@ -5,11 +5,13 @@ public class CannonWeapon : MonoBehaviour, IWeapon
 {
     [Header("Cannon Setup")]
     public GameObject cannonballPrefab; 
-    public Transform firePoint;         
+    public Transform firePoint;
+
+         
     
     [Header("Firing Settings")]
     public float fireForce = 2000f;     
-    public float recoilForce = 500f; 
+    public float recoilForce = 40f; 
 
     // This is the method the player script will trigger when you press the "Melee" button
     public void Attack()
@@ -27,12 +29,12 @@ public class CannonWeapon : MonoBehaviour, IWeapon
         // 2. Apply Recoil to the Player
         // Since the cannon is currently a child of the player's HoldPoint, 
         // GetComponentInParent will find the player's Rigidbody.
-        var playerRb = GetComponentInParent<CharacterController>();
+        var playerKnockback = GetComponentInParent<KnockBack>();
 
-        if (playerRb != null)
+        if (playerKnockback != null)
         {
             Vector3 recoilDirection = -firePoint.forward;
-            playerRb.AddForce(recoilDirection * recoilForce, ForceMode.Impulse);
+            playerKnockback.AddKnockBack(recoilForce, recoilDirection);
         }
     }
 }
--- Assets/Scripts/KnockBack.cs
+++ Assets/Scripts/KnockBack.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class KnockBack : MonoBehaviour
 {
+    public CharacterController controller;
+    private Vector3 velocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(velocity.magnitude > 0.1f)
+        {
+            controller.Move(velocity * Time.deltaTime);
+
+        }
+       
+        velocity = Vector3.Lerp(velocity, Vector3.zero, 4f * Time.deltaTime);
+    }
+
+    public void AddKnockBack(float force, Vector3 direction)
+    {
+        velocity += direction * force;
     }
 }
