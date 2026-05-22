ac1caa2776cd5545ee357ddeb53c9469375360f0
Connor061254 <connor4148@gmail.com>
2026-05-21 20:48:56 -0600

    worked on the inventory system

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEditor.Search;
@@ -5,49 +6,82 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    public List<ItemData> itemList = new List<ItemData>();
+    public ItemData[] inventorySlots = new ItemData[10];
     public int space = 10;
 
     public GameObject objectToDelete;
 
     public GameObject objectToSpawn;
+
+    private OfficialPickupScript pickupScript;
+
+    private readonly KeyCode[] inventoryKeys = new KeyCode[]
+    {
+        KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5,
+        KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0
+    };
    
+
+   void Start()
+    {
+        pickupScript = GetComponent<OfficialPickupScript>();
+    }
     public bool AddItem(ItemData itemToAdd)
     {
-        if(itemList.Count >= space)
+        for(int i = 0; i < inventorySlots.Length; i++)
         {
-            return false;
+            if(inventorySlots[i] == null)
+            {
+                inventorySlots[i] = itemToAdd;
+
+                return true;
+            }
+
         }
 
-        itemList.Add(itemToAdd);
-
-        return true;
+        return false;
     }
 
     public void RemoveItem(ItemData itemToRemove)
     {
-        itemList.Remove(itemToRemove);
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            if(inventorySlots[i] == itemToRemove)
+            {
+                inventorySlots[i] = null;
+                break;
+            }
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        for(int i = 0; i < inventoryKeys.Length; i++)
         {
-            SwapItem();
+            if (Input.GetKeyDown(inventoryKeys[i]))
+            {
+                SwapItem(i);
+            }
         }
     }
 
-    public void SwapItem()
+    public void SwapItem(int slotIndex)
     {
         var pickupScript = this.gameObject.GetComponent<OfficialPickupScript>();
         var spawnPosition = pickupScript.holdPosition;
 
-        Destroy(pickupScript.currentHeldObject);
+       if(pickupScript.currentHeldObject != null)
+        {
+             Destroy(pickupScript.currentHeldObject);
+        }
+      
 
-        ItemData secondItem = itemList[1];
-        objectToSpawn = secondItem.itemGameObject;
+        ItemData targetItem = inventorySlots[slotIndex];
+        objectToSpawn = targetItem.itemGameObject;
 
         GameObject heldItem = Instantiate(objectToSpawn, spawnPosition);
+        heldItem.GetComponent<Rigidbody>().useGravity = false;
+        heldItem.GetComponent<Rigidbody>().isKinematic = true;
 
         heldItem.transform.localPosition = Vector3.zero;
 
@@ -55,5 +89,6 @@ public class InventoryManager : MonoBehaviour
 
         pickupScript.heldObject = heldItem;
         pickupScript.currentHeldObject = heldItem;
+
     }
 }
