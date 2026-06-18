66de86e609540fbfa266eb9280e185f24b3283fd
Connor061254 <connor4148@gmail.com>
2026-06-17 23:27:33 -0600

    worked on some bugs

--- Assets/Scripts/Official Pickup Script.cs
+++ Assets/Scripts/Official Pickup Script.cs
@@ -13,27 +13,21 @@ public class OfficialPickupScript : NetworkBehaviour
     public ItemData itemInfo;
 
     public float dropForce = 2f;
-    Rigidbody rb;
     public Camera mainCamera;
 
     private InteractableItem currentTarget;
-
     public GameObject currentHeldObject;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    void Start() { }
 
     void Update()
     {
         if(!IsOwner) return;
         
-        if (Input.GetKeyDown(KeyCode.E))
-            {
-                AttemptToPickup();
-            }
-    
+        if (Input.GetKeyDown(KeyCode.E) && canPickup)
+        {
+            AttemptToPickup();
+        }
         else if (heldObject != null)
         {
             if (Input.GetKeyDown(KeyCode.Q))
@@ -42,88 +36,121 @@ public class OfficialPickupScript : NetworkBehaviour
             }
         }
     }
+
+    // THE VIRTUAL PARENT: This glues the item to your hand every single frame!
+    void LateUpdate()
+    {
+        if (heldObject != null && currentHeldObject != null)
+        {
+            var itemSettings = heldObject.GetComponent<ItemPickupPosition>();
+            if (itemSettings != null)
+            {
+                heldObject.transform.position = holdPosition.TransformPoint(itemSettings.positionOffset);
+                heldObject.transform.rotation = holdPosition.rotation * Quaternion.Euler(itemSettings.rotationOffset);
+            }
+            else
+            {
+                heldObject.transform.position = holdPosition.position;
+                heldObject.transform.rotation = holdPosition.rotation;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, pickupRange))
         {
-                var hitItem = hit.collider.GetComponent<InteractableItem>();
-                
-                if(hitItem != null)
+            var hitItem = hit.collider.GetComponentInParent<InteractableItem>();
+            
+            if(hitItem != null)
+            {
+                if(currentTarget != hitItem)
                 {
-                    if(currentTarget != hitItem)
-                    {
-                        if(currentTarget != null) currentTarget.HidePrompt();
+                    if(currentTarget != null) currentTarget.HidePrompt();
 
-                        currentTarget = hitItem;
-                        currentTarget.ShowPrompt();
-
-                    }
+                    currentTarget = hitItem;
+                    currentTarget.ShowPrompt();
                 }
-                else
+            }
+            else
+            {
+                if(currentTarget != null)
                 {
-                    if(currentTarget != null)
-                    {
-                        currentTarget.HidePrompt();
-                        currentTarget = null;
-                    }
+                    currentTarget.HidePrompt();
+                    currentTarget = null;
                 }
-        
+            }
         }
     }
+
     void AttemptToPickup()
     {
         int layerMask = 1 << 6;
         layerMask = ~layerMask;
         Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
         RaycastHit hit;
+        
         if (Physics.Raycast(ray, out hit, pickupRange, layerMask))
         {
-            Debug.Log("My Raycast hit: " + hit.collider.name);
-
             if (hit.transform != null && hit.transform.CompareTag("PickUp"))
             {
-                NetworkObject networkObject = hit.transform.GetComponent<NetworkObject>();
-                heldObject = hit.transform.gameObject;
-
-                var itemProp = heldObject.GetComponent<ItemProperties>();
-                var inventory = this.gameObject.GetComponent<InventoryManager>();
-
-                if(inventory != null && itemProp != null && networkObject != null)
-                {
-                    itemInfo = itemProp.referenceData;
-                    
-                    bool wasPickedUp = inventory.AddItem(itemInfo);
-
-                    if (wasPickedUp)
-                    {
-                        RequestPickUpServerRpc(NetworkObject.NetworkObjectId);
-                    }
-                    else
-                    {
-                        heldObject = null;
-                    }
-
-                }
+                NetworkObject networkObject = hit.transform.GetComponentInParent<NetworkObject>();
                 
-            }
+                if (networkObject != null)
+                {
+                    heldObject = networkObject.gameObject;
+                }
 
-            
+                if (heldObject != null)
+                {
+                    var itemProp = heldObject.GetComponent<ItemProperties>();
+                    var inventory = this.gameObject.GetComponent<InventoryManager>();
+
+                    if(inventory != null && itemProp != null && networkObject != null)
+                    {
+                        itemInfo = itemProp.referenceData;
+                        bool wasPickedUp = inventory.AddItem(itemInfo);
+
+                        if (wasPickedUp)
+                        {
+                            Rigidbody[] allRbs = heldObject.GetComponentsInChildren<Rigidbody>();
+                            foreach (Rigidbody r in allRbs)
+                            {
+                                r.isKinematic = true;
+                                r.useGravity = false;
+                            }
+
+                            Collider[] allCols = heldObject.GetComponentsInChildren<Collider>();
+                            foreach (Collider c in allCols)
+                            {
+                                c.enabled = false;
+                            }
+                            
+                            RequestPickUpServerRpc(networkObject.NetworkObjectId);
+                        }
+                        else
+                        {
+                            heldObject = null;
+                        }
+                    }
+                }
+            }
         }
     }
+
     [ServerRpc]   
-     void RequestPickUpServerRpc(ulong itemId, ServerRpcParams rpcParams = default)
+    void RequestPickUpServerRpc(ulong itemId, ServerRpcParams rpcParams = default)
     {
         if(NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(itemId, out NetworkObject item))
         {
-            item.TrySetParent(holdPosition);
-
+            // DELETED the SetParent code here. We only need to give the player ownership!
             item.ChangeOwnership(rpcParams.Receive.SenderClientId);
-
             PerformPickUpClientRpc(itemId);
         }
     }
+
     [ClientRpc]
     void PerformPickUpClientRpc(ulong itemId)
     {
@@ -131,50 +158,50 @@ public class OfficialPickupScript : NetworkBehaviour
         {
             heldObject = item.gameObject;
 
-             if(currentHeldObject != null)
-                        {
-                            currentHeldObject.SetActive(false);
-                        }
-                        
-                        PerformPickup();
+            if(currentHeldObject != null)
+            {
+                currentHeldObject.SetActive(false);
+            }
+            PerformPickup();
         }
     }
 
     void PerformPickup()
     {
+        // We just set this variable so LateUpdate() knows it is time to start gluing it to the hand
         currentHeldObject = heldObject;
-        var itemSettings = heldObject.GetComponent<ItemPickupPosition>();
-        if(itemSettings != null)
+
+        Rigidbody[] allRbs = heldObject.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody r in allRbs)
         {
-            heldObject.transform.localPosition = itemSettings.positionOffset;
-            heldObject.transform.localRotation = Quaternion.Euler(itemSettings.rotationOffset);
+            r.isKinematic = true;
+            r.useGravity = false;
         }
 
-        else
+        Collider[] allCols = heldObject.GetComponentsInChildren<Collider>();
+        foreach (Collider c in allCols)
         {
-            heldObject.transform.localPosition = Vector3.zero;
-            heldObject.transform.localRotation = Quaternion.identity;
+            c.enabled = false;
         }
-       
-        
-        Rigidbody objectRb = heldObject.GetComponent<Rigidbody>();
-        objectRb.isKinematic = true;
-        objectRb.useGravity = false;
     }
 
     void Drop()
     {
         StartCoroutine(DropCooldown());
         var inventory = this.gameObject.GetComponent<InventoryManager>();
-        itemInfo = heldObject.GetComponent<ItemProperties>().referenceData;
-        inventory.RemoveItem(itemInfo);
+        
+        var itemProp = heldObject.GetComponent<ItemProperties>();
+        if (itemProp != null && inventory != null)
+        {
+            itemInfo = itemProp.referenceData;
+            inventory.RemoveItem(itemInfo);
+        }
 
         NetworkObject networkObject = heldObject.GetComponent<NetworkObject>();
         if(networkObject != null)
         {
             RequestDropServerRpc(networkObject.NetworkObjectId, mainCamera.transform.forward);
         }   
-
     }
 
     [ServerRpc]
@@ -182,10 +209,8 @@ public class OfficialPickupScript : NetworkBehaviour
     {
         if(NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(itemId, out NetworkObject item))
         {
-            item.TryRemoveParent();
-
+            // Take ownership back for the Server
             item.RemoveOwnership();
-
             PerformDropClientRpc(itemId, dropDirection);
         }
     }
@@ -193,19 +218,28 @@ public class OfficialPickupScript : NetworkBehaviour
     [ClientRpc]
     void PerformDropClientRpc(ulong itemId, Vector3 dropDirection)
     {
-        
         if(NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(itemId, out NetworkObject item))
         {
             GameObject droppedItem = item.gameObject;
             
-            Rigidbody objectRb = droppedItem.GetComponent<Rigidbody>();
-            if(objectRb != null)
+            Rigidbody[] allRbs = droppedItem.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody r in allRbs)
             {
-                objectRb.isKinematic = false;
-                objectRb.useGravity = true;
-                objectRb.AddForce(dropDirection * dropForce, ForceMode.VelocityChange);
+                r.isKinematic = false;
+                r.useGravity = true;
+                r.AddForce(dropDirection * dropForce, ForceMode.VelocityChange);
+            }
+            
+            Collider[] allCols = droppedItem.GetComponentsInChildren<Collider>();
+            Debug.Log($"Found {allCols.Length} colliders on {droppedItem.name}");
+            foreach (Collider c in allCols)
+            {
+                c.enabled = true;
+                Debug.Log($"Successfully enabled: {c.gameObject.name}");
+
             }
 
+            // Setting these to null instantly tells LateUpdate() to stop holding the item
             if(heldObject == droppedItem)
             {
                 heldObject = null;
@@ -220,8 +254,4 @@ public class OfficialPickupScript : NetworkBehaviour
         yield return new WaitForSeconds(0.5f); 
         canPickup = true;
     }
-
-    
-    
 }
-
