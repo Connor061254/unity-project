0224cb35d85589b83b8f88e3d25fd7d292d60fe4
Connor061254 <connor4148@gmail.com>
2026-07-28 10:11:27 -0600

    update

--- Assets/Scripts/CollectHat.cs
+++ Assets/Scripts/CollectHat.cs
@@ -5,18 +5,15 @@ public class CollectHat : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("colission triggered");
+        Debug.Log($"colission triggered with {collision.gameObject.name}");
         if (collision.gameObject.GetComponent<PlayerController>())
         {
             Debug.Log($"{collision.gameObject.name} has a PlayerController");
             var playerPoints = collision.gameObject.GetComponent<Points>();
 
             ++playerPoints.points.Value;
+            
+            Destroy(gameObject);
         }
-
-        Destroy(gameObject);
-      
     }
-
-
 }
