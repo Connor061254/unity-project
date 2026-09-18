d04a9f74613ff0350e581dc219f07eabbcd4f8a8
Connor061254 <connor4148@gmail.com>
2026-09-17 20:59:50 -0600

    working on champ select UI

---
+++ Assets/Scripts/ChampionSelectionUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class ChampionSelectionUI : MonoBehaviour
+{
+    public static int SavedChampChoice = -1;
+
+    public void SelectTallPirate()
+    {
+        
+    }
+
+    public void SelectFatPirate()
+    {
+        
+    }
+
+    public void SelectWomenPirate()
+    {
+        
+    }
+}
